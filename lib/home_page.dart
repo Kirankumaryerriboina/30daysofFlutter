be7215ee26d6fd81ac065bd47a0962d1ed6b96ca
Kiran Kumar <kirankumar@Kirans-MacBook-Air-2.local>
2023-08-27 10:31:58 +0530

    Day3 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Kiran Kumar";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days course by $name."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
